ff3e60b5382d2f014ce606611b21d48afa91aa4a
Henry Kiehna <jhkieha@gmail.com>
2022-12-01 09:45:42 -0500

    day 1 part 2

--- day01.ts
+++ day01.ts
@@ -1,3 +1,5 @@
+// Part 1
+
 export function mostCaloriesCarriedBySingleElf(input: string) {
   const sums = input
     .split(/\n\n/)
@@ -5,4 +7,18 @@ export function mostCaloriesCarriedBySingleElf(input: string) {
     .map((g) => g.reduce((a, n) => (a += n)), 0);
 
   return Math.max(...sums);
+}
+
+// Part 2
+
+export function totalCaloriesOfTopThree(input: string) {
+  const sums = input
+    .split(/\n\n/)
+    .map((g) => g.split(/\n/).map((n) => Number(n)))
+    .map((g) => g.reduce((a, n) => (a += n)), 0);
+
+  return sums
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, n) => (a += n), 0);
 }
